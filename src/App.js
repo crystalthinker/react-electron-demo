3a7beafab4567516772b6f8467f19d2ed1f9b6db
abhilash-kr <abhilash.kr@aot-technologies.com>
2018-07-19 15:28:25 +0530

    updated to weather app with electron

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Weather from "./components/weather/weather.component";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App Container">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React Weather App with electron</h1>
+          <h1 className="App-title">Welcome to Weather app</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Weather/>
       </div>
     );
   }
